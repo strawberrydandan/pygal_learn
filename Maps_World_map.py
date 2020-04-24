719d12749434a7a581b9a4ac4c167ef950186247
wudan <pangdanyep@163.com>
2020-04-24 09:06:47 +0800

    pygal_map类型统计图的使用学习

---
+++ Maps_World_map.py
@@ -0,0 +1,13 @@
+#!/user/bin/python3
+# -*- coding: utf-8 -*-
+import pygal
+
+worldmap_chart = pygal.maps.world.World()
+worldmap_chart.title = 'Some countries'
+worldmap_chart.add('F countries', ['fr', 'fi'])
+worldmap_chart.add('M countries', ['ma', 'mc', 'md', 'me', 'mg',
+                                   'mk', 'ml', 'mm', 'mn', 'mo',
+                                   'mr', 'mt', 'mu', 'mv', 'mw',
+                                   'mx', 'my', 'mz'])
+worldmap_chart.add('U countries', ['ua', 'ug', 'us', 'uy', 'uz'])
+worldmap_chart.render_to_file('./image/world_map.svg')
